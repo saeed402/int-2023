57ce6f0bb0d58eba35054b97140599c845e1b9e5
saeed402 <139351697+saeed402@users.noreply.github.com>
2023-08-20 20:12:00 +0500

    Update listing_final_test.sh

--- listing_final_test.sh
+++ listing_final_test.sh
@@ -8,10 +8,30 @@ fi
 directory="$1"
 email_address="$2"
 
+# Get file size limit from the user
+read -p "Enter the minimum file size in KB: " min_size_kb
+
 # Generate file listing with extended details
 find "$directory" -type f -exec ls -lh --time-style=long-iso --block-size=1 {} + | \
 awk '{split($6, a, "-"); print $1 " , " $3 " , " $5 " , " a[1]"-"a[2]"-"a[3] " , " $NF }' > treefile.csv
 
-# Filter files based on size and extension
-awk -F ' , ' 'BEGIN { OFS=" , " } $3 > 100000 && $NF ~ /\.txt$/ { print }' treefile.csv > filtered_files.csv
+# Debugging output
+echo "Debugging Output:"
+echo "Min Size: $min_size_kb"
+echo "File Listing:"
+cat treefile.csv
 
+# Filter files based on user input size and extension
+awk -F ' , ' -v min_size="$min_size_kb" 'BEGIN { OFS=" , " } $3 > min_size * 1024 && $NF ~ /\.txt$/ { print }' treefile.csv > filtered_files.csv
+
+# Debugging output
+echo "Filtered Files:"
+cat filtered_files.csv
+
+# Run the Python script to send the email
+python3 send_email.py "$email_address" "$min_size_kb"
+
+# Purge selected files
+awk -F ' , ' '{ print $NF }' filtered_files.csv | xargs rm -f
+
+echo "Program completed."
