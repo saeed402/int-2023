a0387f45a6fd4d66eb761e92a74594ce1a9615b5
saeed402 <139351697+saeed402@users.noreply.github.com>
2023-08-21 11:11:32 +0500

    Update listing_final_test.sh

--- listing_final_test.sh
+++ listing_final_test.sh
@@ -12,6 +12,7 @@ email_address="$2"
 read -p "Enter the minimum file size in KB: " min_size_kb
 
 # Generate file listing with extended details
+echo "Attempting to run listing script:"
 find "$directory" -type f -exec ls -lh --time-style=long-iso --block-size=1 {} + | \
 awk '{split($6, a, "-"); print $1 " , " $3 " , " $5 " , " a[1]"-"a[2]"-"a[3] " , " $NF }' > treefile.csv
 
@@ -22,6 +23,7 @@ echo "File Listing:"
 cat treefile.csv
 
 # Filter files based on user input size and extension
+echo "Attempting to run filter script:"
 awk -F ' , ' -v min_size="$min_size_kb" 'BEGIN { OFS=" , " } $3 > min_size * 1024 && $NF ~ /\.txt$/ { print }' treefile.csv > filtered_files.csv
 
 # Debugging output
@@ -29,9 +31,18 @@ echo "Filtered Files:"
 cat filtered_files.csv
 
 # Run the Python script to send the email
+echo "Attempting to run email script:"
 python3 send_email.py "$email_address" "$min_size_kb"
 
 # Purge selected files
+echo "Attempting to run purge script:"
 awk -F ' , ' '{ print $NF }' filtered_files.csv | xargs rm -f
 
-echo "Program completed."
+# Check if file purging was successful
+if [ $? -eq 0 ]; then
+    echo "Files purged successfully."
+    exit 0
+else
+    echo "Error occurred while purging files."
+    exit 1
+fi
